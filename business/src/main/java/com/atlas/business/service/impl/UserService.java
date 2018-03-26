b69dd0fb6be4012a3f3d365b87b0300a9cffb838
任飞 <renfei@yingxinhuitong.com>
2018-03-26 18:00:31 +0800

    加kotlin依赖

--- business/src/main/java/com/atlas/business/service/impl/UserService.java
+++ business/src/main/java/com/atlas/business/service/impl/UserService.java
@@ -9,6 +9,7 @@ import com.atlas.framework.web.session.UserSessionManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
